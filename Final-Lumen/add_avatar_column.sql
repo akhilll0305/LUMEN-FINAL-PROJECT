5222abac15eb7970595357e5a4f764fb6a65142d
akhil_007 <r.akhilreddy007@gmail.com>
2025-11-15 14:37:52 +0530

    Final

--- Final-Lumen/add_avatar_column.sql
+++
@@ -1,5 +0,0 @@
--- Add avatar_url column to users_consumer table
-ALTER TABLE users_consumer ADD COLUMN IF NOT EXISTS avatar_url VARCHAR;
-
--- Add avatar_url column to users_business table
-ALTER TABLE users_business ADD COLUMN IF NOT EXISTS avatar_url VARCHAR;
